d363c14f95d55b7bb6b62c9e724dc15742101ceb
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-03 21:38:15 +0000

    rewrite loops and print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0-9):
-    for j in range(i + 1, 9):
-        if i == (8, 9) and j == (8, 9):
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != j:
             print("{}{}".format(i, j), end=", ")
-        else:
-            print("{}{}".format(i, j))
