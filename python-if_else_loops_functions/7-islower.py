78e05c01cb3662a44da2dc03c60aee728f292492
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-03 22:09:39 +0000

    edit if statement

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-    
-    acsii = range(chr(97, 123))
-    
-    if c == ascii or range(0-9):
+
+    if 'a' <= c <= 'z':
         return True
     else:
         return False
