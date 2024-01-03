72e434dd249af70981eb0ab07c67bbb983e98356
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-03 22:31:03 +0000

    change if else and test

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,11 @@
 
 def uppercase(str):
 
+    new_str = ""
     for char in str:
-        chr(ord(char) - 32)
-    return str
-    print("{}".format(str))
+        if 'a' <= char <= 'z':
+            new_str += chr(ord(char) - 32)
+        else:
+            new_str += char
+    return new_str
+    print("{}".format(new_str))
