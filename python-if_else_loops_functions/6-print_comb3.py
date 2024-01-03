04c9c9cfaac8a26583f97dbc0ab13f863db18fee
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-03 21:26:06 +0000

    if else statments

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 
 for i in range(0-9):
     for j in range(i + 1, 9):
-        print("{}{}".format(i, j), end=", ")
-        if (i,j) == (8,9):
-            print()
+        if i == (8, 9) and j == (8, 9):
+            print("{}{}".format(i, j), end=", ")
+        else:
+            print("{}{}".format(i, j))
