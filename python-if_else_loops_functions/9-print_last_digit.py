aa5e6b5a2da79202ebb1005b37d34412b36d98af
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-04 02:11:15 +0000

    test for negative case

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
+    
+    if number < 0:
+        number = number * -1
+    
     last_dig = number % 10
     print("{}".format(last_dig), end="")
     return last_dig
