0ceaa098f91b5b99acc9d7a639db03582603b1b6
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-04 21:47:33 +0000

    print arg instead of argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,9 +6,11 @@ counter = 1
 if __name__ == "__main__":
     if len(sys.argv) < 1:
         print("{} arguments.".format(len(sys.argv)), end="")
+    elif len(sys.argv) == 1:
+        print("{} argument: ".format(len(sys.argv)))
     else:
         print("{} arguments: ".format(len(sys.argv)))
 
     for arg in sys.argv:
-        print("{}: {}".format(counter, sys.argv))
+        print("{}: {}".format(counter, arg))
         counter += 1
