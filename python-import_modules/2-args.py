1c3be52ea5eb248dfa3c4b4dbeb406078eca8985
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-04 21:50:53 +0000

    edit to rm proram name from count

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 counter = 1
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        print("{} arguments.".format(len(sys.argv)), end="")
-    elif len(sys.argv) == 1:
-        print("{} argument: ".format(len(sys.argv)))
+    if len(sys.argv) < 2:
+        print("{} arguments.".format(len(sys.argv)-1), end="")
+    elif len(sys.argv) == 2:
+        print("{} argument: ".format(len(sys.argv)-1))
     else:
-        print("{} arguments: ".format(len(sys.argv)))
+        print("{} arguments: ".format(len(sys.argv)-1))
 
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
         print("{}: {}".format(counter, arg))
         counter += 1
