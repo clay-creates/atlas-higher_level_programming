8aacf5b1e30232c052e9cc9d7fc5a65735ef0583
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-04 20:42:26 +0000

    make 1-file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
+    import calculator_1
+
+a = 10
+b = 5
+
+print("{}".format(add(a, b)))
+print("{}".format(sub(a, b)))
+print("{}".format(mul(a, b)))
+print("{}".format(div(a, b)))
