0287dacfc989c06a004b54b9a9a1bfdd3f772f18
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-04 21:42:35 +0000

    make 2-file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+import sys
 
+counter = 1
+
+if __name__ == "__main__":
+    if len(sys.argv) < 1:
+        print("{} arguments.".format(len(sys.argv)), end="")
+    else:
+        print("{} arguments: ".format(len(sys.argv)))
+
+    for arg in sys.argv:
+        print("{}: {}".format(counter, sys.argv))
+        counter += 1
