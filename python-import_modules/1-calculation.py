f8887f0983db437e76eb1cbcfb0a1884503ab820
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-04 21:17:48 +0000

    fix ordering to test output

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    from calculator_1 import add; sub; mul; div;
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-print("{}".format(add(a, b)))
-print("{}".format(sub(a, b)))
-print("{}".format(mul(a, b)))
-print("{}".format(div(a, b)))
+    print("{}".format(add(a, b)))
+    print("{}".format(sub(a, b)))
+    print("{}".format(mul(a, b)))
+    print("{}".format(div(a, b)))
