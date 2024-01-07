cb29eb502c38a169c91d30ea212903fd3d73e0df
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-07 22:17:37 +0000

    test with >=

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         new = my_list
         return new
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         new = my_list
         return new
 
