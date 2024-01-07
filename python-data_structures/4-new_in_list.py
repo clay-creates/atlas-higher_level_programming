f7e4f47849408246224911bfbcd7a2bc663b6f90
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-07 22:21:16 +0000

    test with return mylist

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    new = None
-
     if idx < 0:
-        new = my_list
-        return new
+        return my_list
     elif idx >= len(my_list):
-        new = my_list
-        return new
+        return my_list
 
-    new = my_list
+    new = my_list[:]
 
     for i in new:
         if i == idx:
