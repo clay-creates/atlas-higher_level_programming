bf48d00c3fc14c983280b8dc18bb50fc9e061967
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-07 23:59:52 +0000

    rm loop, use del function

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,8 +4,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
 
-    for i in my_list:
-        if i == idx:
-            i = None
+    del my_list[idx]
 
     return my_list
