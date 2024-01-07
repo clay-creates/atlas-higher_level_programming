63f80393fb2761ffc3434a6dafc3fd866df5a07b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-07 23:40:04 +0000

    rm concat, revise value assignment

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,11 +2,11 @@
 
 def divisible_by_2(my_list=[]):
     new_list = ''
-    
+
     for i in my_list:
         if i % 2 == 0:
-            new_list += True
+            new_list[i] = True
         else:
-            new_list += False
+            new_list[i] = False
 
     return new_list
