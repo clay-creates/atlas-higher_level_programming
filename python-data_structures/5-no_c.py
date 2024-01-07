de1b3aefdb33bdc48acaf22b1aab4d0c10a953d6
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-07 22:42:19 +0000

    fix for loop, adding new_string var

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    new_string = ''
     for char in my_string:
-        if char == 'c' or char == 'C':
-            char = ''
+        if char != 'c' and char != 'C':
+            new_string += char
 
-    return my_string
+    return new_string
