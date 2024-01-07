66820065d55baf073f2a8082b54c0decf4715228
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-07 22:12:08 +0000

    rm range function, whitespace

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,16 +2,16 @@
 
 def new_in_list(my_list, idx, element):
     new = None
-    
+
     if idx < 0:
         new = my_list
         return new
-    elif idx > len(range(my_list)):
+    elif idx > len(my_list):
         new = my_list
         return new
-    
+
     new = my_list
-    
-    for i in range(new):
+
+    for i in new:
         if i == idx:
             new[i] = element
