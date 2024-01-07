60d5987b0b1c966339bf702b4b4adf1520fecd19
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-07 23:25:33 +0000

    rm range function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    big_int = 0
-    
     if my_list is None:
         return None
-    
-    for i in range(my_list):
+
+    big_int = 0
+
+    for i in my_list:
         if i > big_int:
             big_int = i
 
