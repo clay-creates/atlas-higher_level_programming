4143fd9e83bbd052ae452eae16114dbe6526992c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-08 23:50:25 +0000

    split sort and make new dict

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
 
-    print("{}".format(sorted(a_dictionary)))
+    sort_items = sorted(a_dictionary.items())
+    sorted_dict = dict(sort_items)
+    print("{}".format(sorted_dict))
