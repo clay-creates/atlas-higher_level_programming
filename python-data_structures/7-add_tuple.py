120f2b49c73db0390edb4410291221776c875908
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-08 06:31:37 +0000

    test with new_tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a_sum = sum(tuple_a[:2])
-    b_sum = sum(tuple_b[:2])
+    a_sum = sum(tuple_a[:2]) + (0, 0)
+    b_sum = sum(tuple_b[:2]) + (0, 0)
 
-    return (a_sum, b_sum)
+    new_tuple = (a[0] + b[0], a[1] + b[1])
+
+    return new_tuple
