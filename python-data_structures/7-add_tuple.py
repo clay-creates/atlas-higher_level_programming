5ba05f233c2e153dc5ac0225d5c2ef76ad2d1a6c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-08 06:36:15 +0000

    rm sum and :d from first vars

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a_sum = sum(tuple_a[:2]) + (0, 0)
-    b_sum = sum(tuple_b[:2]) + (0, 0)
+    a_sum = tuple_a + (0, 0)
+    b_sum = tuple_b + (0, 0)
 
     new_a = a_sum[0] + b_sum[0]
     new_b = a_sum[1] + b_sum[1]
