3b098121de8175b7df05ce89ad09ea03cab6c2f8
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-08 22:49:29 +0000

    make 0-file

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        return None
+
+    new_matrix = [[i ** i for i in row] for row in matrix]
+
+    return new_matrix
