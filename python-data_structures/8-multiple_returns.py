e3228a1d8cf97b26db76b2767625ece17771d636
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-08 06:43:28 +0000

    pycode

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
     if sentence is None:
         sentence = None
-    elif sentence != None:
+    elif sentence is not None:
         length = len(sentence)
     else:
         lenght = 0
