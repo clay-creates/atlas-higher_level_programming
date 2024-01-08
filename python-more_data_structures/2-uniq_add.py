aa91c7238f510de9f338162225fec3fb23eafd4d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-08 23:10:32 +0000

    test for multipling elements

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    uniq_sum = []
+    uniq_sum = set(my_list)
 
-    for i in set(my_list):
-        uniq_sum += i
+    for i in uniq_sum:
+        uniq_sum *= i
 
     return uniq_sum
