e9bbac2e054541c8343e33b5cdaac538ddde27be
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-09 20:28:11 +0000

    iterate with range(len))

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if my_list is None:
-        return None
-    elif search is None or replace is None:
-        return my_list
 
-    new_list = my_list
+    new_list = list(my_list)
 
-    for element in my_list:
-        if element == search:
-            new_list[element] = replace
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
 
     return new_list
