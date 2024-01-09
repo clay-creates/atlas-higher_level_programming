86daba5649e46c5ac2aac60f2e03981f5bc6751c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-09 20:55:30 +0000

    simplify, return using ^

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    set1 = set(set_1)
-    set2 = set(set_2)
 
-    diff = [i for i in set1 if i not in set2]
-    return diff
+    return set_1 ^ set_2
