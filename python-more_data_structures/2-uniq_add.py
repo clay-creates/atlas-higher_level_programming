5a5d47aa90f7220d9003a0378400825b3b6f411e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-09 20:36:19 +0000

    simplify using sum function

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    uniq_nums = set(my_list)
-    sum = 1
-    for i in uniq_nums:
-        sum *= i
 
-    return sum
+    uniq_list = set(my_list)
+    return sum(uniq_list)
