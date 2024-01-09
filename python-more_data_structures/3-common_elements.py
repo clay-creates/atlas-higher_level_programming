8be1ff0a675f6502e599342052f02ffde784dc1d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-09 20:43:26 +0000

    revert

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
+    set1 = set(set_1)
+    set2 = set(set_2)
 
-    return set_1 & set_2
+    common = set1.intersection(set2)
+
+    return common
