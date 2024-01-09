ca5563066b380085b5517cf247557780ab8de105
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-09 20:22:15 +0000

    fix formula

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     if matrix is None:
         return None
 
-    new_matrix = [[i ** i for i in row] for row in matrix]
+    new_matrix = [[i ** 2 for i in row] for row in matrix]
 
     return new_matrix
