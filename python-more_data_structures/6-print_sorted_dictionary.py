3ce5d91f9ffa8f2d6a256b1b92d700b84cd86dfb
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-09 20:58:31 +0000

    change items to keys, iterate and edit print

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
 
-    sort_items = sorted(a_dictionary.items())
-    sorted_dict = dict(sort_items)
-    print("{}".format(sorted_dict))
+    sort_items = sorted(a_dictionary.keys())
+    for i in sort_items:
+        print("{}: {}".format(i, a_dictionary[i]))
