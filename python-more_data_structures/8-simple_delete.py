8e1bbc7a35d85ffdf0f7f3d2aab3f53d140830fb
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-09 00:13:35 +0000

    rm for loop and edit logic

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,13 +2,9 @@
 
 def simple_delete(a_dictionary, key=""):
 
-    if key is None:
-        return a_dictionary
-
-    for i in a_dictionary:
-        if a_dictionary[i] == key:
-            del(a_dictionary[i])
-        else:
-            a_dictionary += key
+    if key in a_dictionary:
+        del a_dictionary[key]
+    else:
+        a_dictionary[key] = ""
 
     return a_dictionary
