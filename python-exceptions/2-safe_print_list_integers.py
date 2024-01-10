56dc7168359402d0758766c6ac133e269749cfbd
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-10 02:08:53 +0000

    test iterating mylist

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@
 def safe_print_list_integers(my_list=[], x=0):
 
     printed = 0
-    for i in range(x):
+    for i in range(len(my_list)):
         try:
             print("{:d}".format(my_list[i], end=""))
             printed += 1
