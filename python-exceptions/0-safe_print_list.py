8014b9df8070ae54fa4f748ea67cd65ee5a47709
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-10 00:49:58 +0000

    test

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[i], end=""))
             printed += 1
         except IndexError:
-            print()
+            print("")
             return printed
 
     print()
