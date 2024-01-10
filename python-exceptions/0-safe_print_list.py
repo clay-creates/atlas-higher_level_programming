7dcf5909c8dad38d6b01624b23a5fca7c044c0b3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-10 00:47:34 +0000

    rewrite

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,21 +2,14 @@
 
 def safe_print_list(my_list=[], x=0):
 
-    list_len = 0
-    for i in my_list:
-        list_len += 1
-
     printed = 0
-
-    try: 
-        for i in range(x):
-            print("{}".format(my_list[i]), end=' ')
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i], end=""))
             printed += 1
-    except IndexError:
-        for i in range(list_len):
-            print("{}".format(my_list[i]), end=' ')
-            printed += 1
+        except IndexError:
+            print()
+            return printed
 
-    print ()
-
+    print()
     return printed
