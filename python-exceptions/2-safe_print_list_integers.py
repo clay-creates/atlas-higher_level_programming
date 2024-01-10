9506a6344a3c23d6908c6d40dd1a2f51fe26239b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-10 02:13:34 +0000

    pass to continue

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-
-    printed = 0
+    counter = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i], end=""))
-            printed += 1
+            counter += 1
         except (TypeError, ValueError):
-            pass
+            continue
 
     print()
-    return printed
+    return counter
