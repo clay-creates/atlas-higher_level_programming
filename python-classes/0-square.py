c3599a27320ba7c903732e432b06549e9dda7645
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-11 23:21:04 +0000

    improper indentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-    """This module works to define the Square class
-    """
+"""This module works to define the Square class
+"""
 
 class Square:
     """This class defines the class Square and future square objects.
