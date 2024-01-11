dbc3a1e798bdb9ba1929e040437e45b1da7db1c3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-11 23:19:31 +0000

    module documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+    """This module works to define the Square class
+    """
+
 class Square:
-    """This class defines the shape of a square.
+    """This class defines the class Square and future square objects.
     """
     pass
