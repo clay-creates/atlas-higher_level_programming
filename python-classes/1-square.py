6ccd5ea7c755b4aee4424bb723181eba8b49a990
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-12 00:43:06 +0000

    test with size defined in square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,15 +4,7 @@
     """
 
 
-class Square:
+class Square(__size=0):
     """This defines the class: Square
     """
     pass
-
-def __init__(self, __size):
-    """This is the definition of the init method
-
-    Args:
-        __size: This is the private attribute of size 
-    """
-    self.size = __size
