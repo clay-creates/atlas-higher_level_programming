267956f000cc02262592b38327ec7677e57ed41d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-12 00:44:07 +0000

    revert deleted text

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,3 +8,11 @@ class Square(__size=0):
     """This defines the class: Square
     """
     pass
+
+def __init__(self, __size):
+    """This is the definition of the init method
+
+    Args:
+        __size: This is the private attribute of size 
+    """
+    self.size = __size
