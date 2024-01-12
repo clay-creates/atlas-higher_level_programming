41665822823aa5af62d9c3cb4af5e247f0972973
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-12 04:21:18 +0000

    rm extra if conditions in __init__

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -16,13 +16,8 @@ class Square:
         Args:
             size (int): size of the Square
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            self.__position = position
+        self.__size = size
+        self.__position = position
 
     def area(self):
         """
