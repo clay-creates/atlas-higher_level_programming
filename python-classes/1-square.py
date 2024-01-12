9b85fc9bf3eaeccab304a0d0be700ebef0ac5e1d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-12 00:40:09 +0000

    make 1-file

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""This module defines the square class.
+    """
+
+
+class Square:
+    """This defines the class: Square
+    """
+    pass
+
+def __init__(self, __size):
+    """This is the definition of the init method
+
+    Args:
+        __size: This is the private attribute of size 
+    """
+    self.size = __size
