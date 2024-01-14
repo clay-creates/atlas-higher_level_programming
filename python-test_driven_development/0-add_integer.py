b5f4bf8fafe0af512269a667e80134ad9534174d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-14 03:15:36 +0000

    make 0-file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int:
+        raise TypeError("b must be an integer")
     
+    if type(a) is float:
+        a = int(a)
+    elif type(b) is float:
+        b = int(b)
+        
+    return a + b
