1dfba1de759383890917f90074b58e715be19845
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-14 03:59:02 +0000

    fix order of if and document

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
-
+"""
+This module contains the add_integer method    
+"""
 def add_integer(a, b=98):
-    if type(a) is not int:
-        raise TypeError("a must be an integer")
-    elif type(b) is not int:
-        raise TypeError("b must be an integer")
-    
+    """
+    This method adds two integers together. If given a float,
+    it will translate them to integers and return the sum.
+
+    Args:
+        a (int): first value
+        b (int, optional): second value. Defaults to 98.
+
+    Raises:
+        TypeError: a value must be an integer
+        TypeError: b value must be an integer
+
+    Returns:
+        int: sum of two values passed to method
+    """
+
     if type(a) is float:
         a = int(a)
     elif type(b) is float:
         b = int(b)
-        
+    elif type(a) is not int:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int:
+        raise TypeError("b must be an integer")
+
     return a + b
