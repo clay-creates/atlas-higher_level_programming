f74845267c00d8b89c94404ad63ca88d245bff69
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-15 20:01:53 +0000

    revert

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -21,9 +21,13 @@ def add_integer(a, b=98):
         int: sum of two values passed to method
     """
 
-    if type(a) is not [int, float]:
+    if type(a) is float:
+        a = int(a)
+    elif type(b) is float:
+        b = int(b)
+    elif type(a) is not int:
         raise TypeError("a must be an integer")
-    elif type(b) is not [int, float]:
+    elif type(b) is not int:
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    return a + b
