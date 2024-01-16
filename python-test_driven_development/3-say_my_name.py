a4162aef55d92a45565f92884219d897892cb2ca
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 01:58:46 +0000

    make 3-file.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module contains the say_my_name method
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Method to print out both first name and last name given
+
+    Args:
+        first_name (string): first name given.
+        last_name (str, optional): last name given. Defaults to "".
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}").format(first_name, last_name)
