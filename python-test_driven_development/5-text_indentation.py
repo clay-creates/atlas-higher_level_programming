8312e07be688db890a4f15f6761f28bcb5575669
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 05:32:56 +0000

    make 5.py and initial test

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module contains implementation of the text_indentation method
+"""
+
+def text_indentation(text):
+
+    if not isinstance(str):
+        raise TypeError("text must be a string")
+    
+    for char in len(range(text)):
+        if text[char] == ['.', '?', ':']:
+            print("\n")
+        print(char, end="")
+        
