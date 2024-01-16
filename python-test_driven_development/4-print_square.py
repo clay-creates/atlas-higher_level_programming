cbd23b92f231ae17df07989494d899b9e0e2dc65
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 05:05:04 +0000

    pystyle

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,12 +4,12 @@
 This module defines a method Print_Square
 """
 
+
 def print_square(size):
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size mist be >= 0")
-    
+
     print("{}".format("#") for i in size for row in size)
-    
