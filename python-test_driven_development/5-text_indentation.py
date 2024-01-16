a19690b47791f9ba6a8089f20d76bce98100fb16
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 05:44:53 +0000

    add tests, document

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,13 +4,21 @@
 This module contains implementation of the text_indentation method
 """
 
-def text_indentation(text):
 
+def text_indentation(text):
+    """
+    This method prints two newlines after these characters: ['.', '?', ':']
+
+    Args:
+        text (str): text to be printed out and indented
+
+    Raises:
+        TypeError: text must be a string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     for char in text:
         if char in ['.', '?', ':']:
-            print("\n")
+            print("char\n")
         print(char, end="")
-        
