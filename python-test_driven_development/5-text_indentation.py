b81835c6bae8fd1642e5c183338f01937fc9757f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 05:33:46 +0000

    edit to isinstance

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,7 @@ This module contains implementation of the text_indentation method
 
 def text_indentation(text):
 
-    if not isinstance(str):
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
     
     for char in len(range(text)):
