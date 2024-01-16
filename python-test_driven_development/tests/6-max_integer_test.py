34b9281ff74614dbb953254c2a2d1830b3125da2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 21:11:55 +0000

    mv expected to outer ()

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -16,25 +16,25 @@ class TestMaxInteger(unittest.TestCase):
         """
         Test case for list with positive and negative integers
         """
-        self.assertEqual(max_integer([-1, 2, -3, 4, -5], 4))
+        self.assertEqual(max_integer([-1, 2, -3, 4, -5]), 4)
 
     def test_max_end(self):
         """
         Test case for max int at end of list
         """
-        self.assertEqual(max_integer([1, 2, 3, 4, 5], 5))
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
     def test_max_start(self):
         """
         Test case for max int at start of list
         """
-        self.assertEqual(max_integer([5, 4, 3, 2, 1], 5))
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
 
     def test_max_mid(self):
         """
         Test case for max int in middle of list
         """
-        self.assertEqual(max_integer([1, 2, 5, 3, 4], 5))
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
 
     def test_single_negative(self):
         """
@@ -46,7 +46,7 @@ class TestMaxInteger(unittest.TestCase):
         """
         Test case for list of only negative integers
         """
-        self.assertEqual(max_integer([-1, -2, -3, -4, -5], -1))
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
 
     def test_none(self):
         """
