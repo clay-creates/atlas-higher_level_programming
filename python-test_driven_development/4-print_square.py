45b590c1aae822de192c828ae4c971190a0db12f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 05:10:14 +0000

    typo

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -20,7 +20,7 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
-        raise ValueError("size mist be >= 0")
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         for j in range(size):
