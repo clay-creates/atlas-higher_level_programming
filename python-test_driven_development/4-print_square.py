13f02f8287bf6f54e3d1ab7fa131400e85e47086
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 05:09:16 +0000

    document

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,6 +6,16 @@ This module defines a method Print_Square
 
 
 def print_square(size):
+    """
+    Prints a square of given size, using "#"
+
+    Args:
+        size (int): size of square to print
+
+    Raises:
+        TypeError: size must be int
+        ValueError: size must be positive
+    """
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
