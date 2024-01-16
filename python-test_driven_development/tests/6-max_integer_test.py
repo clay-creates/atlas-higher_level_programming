b3504065c026a1445a01a9300c52da6366f194d7
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-16 21:09:32 +0000

    remake unittests, proper synatx

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -12,13 +12,43 @@ class TestMaxInteger(unittest.TestCase):
     Unittest for method: max_integer
     """
 
-    def TestExpected(self):
+    def test_mixed(self):
         """
-        Test the expected case, a list of positive integers
+        Test case for list with positive and negative integers
+        """
+        self.assertEqual(max_integer([-1, 2, -3, 4, -5], 4))
+
+    def test_max_end(self):
+        """
+        Test case for max int at end of list
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4, 5], 5))
+
+    def test_max_start(self):
+        """
+        Test case for max int at start of list
         """
         self.assertEqual(max_integer([5, 4, 3, 2, 1], 5))
 
-    def TestNoneList(self):
+    def test_max_mid(self):
+        """
+        Test case for max int in middle of list
+        """
+        self.assertEqual(max_integer([1, 2, 5, 3, 4], 5))
+
+    def test_single_negative(self):
+        """
+        Test case for a single negative integer in list
+        """
+        self.assertEqual(max_integer([]))
+
+    def test_all_negative(self):
+        """
+        Test case for list of only negative integers
+        """
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5], -1))
+
+    def test_none(self):
         """
         Test case of empty list
         """
