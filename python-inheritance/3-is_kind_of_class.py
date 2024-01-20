c41877e5bcf64e0da4bb371032dbb100f68fa2e5
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-20 23:31:20 +0000

    document

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,16 @@ This method contains the is_kind_of_class method
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Method that checks if object is instance of class/subclass of given class
+
+    Args:
+        obj (object): object to be checked
+        a_class (class): class to check against
+
+    Returns:
+        Bool: returns true if is same class/subclass, false otherwise
+    """
     if isinstance (type(obj), a_class):
         return True
     else:
