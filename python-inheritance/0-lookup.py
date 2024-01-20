5e4af75a1ce55957bb024a6b9e01c1aa67d1d5ff
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-20 05:10:47 +0000

    make 0-file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines the lookup(obj) method
+"""
+
+
+def lookup(obj):
+    """
+    Method that returns attributes and methods of given object
+
+    Args:
+        obj (object): any version of an object
+
+    Returns:
+        list: list containing attributes and methods of obj
+    """
+    return [dir(obj)]
