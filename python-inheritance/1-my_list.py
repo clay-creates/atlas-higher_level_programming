fd89a48fe0fc064dfd471dd56834965308144b63
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-20 21:34:36 +0000

    make 1-file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module with MyList subclass inheriting from built-in list class
+"""
+
+
+class MyList(list):
+    """
+    Subclass inheriting from list built in class
+
+    Args:
+        list (class): superclass that our class inherits from
+    """
+
+    def print_sorted(self):
+        """
+        Method to print sorted list
+        """
+        print(list.sort())
