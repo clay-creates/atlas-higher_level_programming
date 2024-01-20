599efa308bcd8c6eede8b1abf4838ddf29fd1ff3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-20 23:14:33 +0000

    make 2-file

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module defines the is_same_class method
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Method that checks if an object is an instance of given class
+
+    Args:
+        obj (object): object to be checked
+        a_class (class): class to see if is instance
+
+    Returns:
+        Bool: returns True if is instance, returns False otherwise
+    """
+    if obj is a_class:
+        return True
+    else:
+        return False
