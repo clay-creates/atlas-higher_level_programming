dd4a1a626f8e90d295968db0e5114ca67c2501fc
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-21 01:12:41 +0000

    make 6-file

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module creates a new class: BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        Method to calculate area of given geometry
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        try:
+            pass
+        except:
+            raise Exception("area() is not implemented")
+        
