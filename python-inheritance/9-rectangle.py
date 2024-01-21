9da98ab8cf8e2f8285f8011b4cdc7ea911141178
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-21 22:31:53 +0000

    revert, wrong file

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -37,4 +37,4 @@ class Rectangle(BaseGeometry):
         """
         Magic method to print the representation of Rectangle
         """
-        print("[Square] {}/{}".format(self.__width, self.__height))
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
