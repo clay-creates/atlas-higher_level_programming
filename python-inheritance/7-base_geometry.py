01214d2c8d71fd92085e763adfa75e5ae66edeae
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-21 08:18:44 +0000

    revert

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -30,6 +30,6 @@ class BaseGeometry:
             ValueError: name must be > 0
         """
         if not isinstance(name, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
