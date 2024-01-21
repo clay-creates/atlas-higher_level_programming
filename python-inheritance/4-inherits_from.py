2b7ddb14c82a14480af0196d891acbaa9b7b1fe3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-21 00:57:26 +0000

    make 4-file

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pyton3
+"""
+This module contains the inherits_from method
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    This method checks if the object is a subclass of given class
+
+    Args:
+        obj (object): object to check
+        a_class (class): class to check against
+
+    Returns:
+        Bool: return true if is instance, false if not
+    """
+    if isinstance(super(obj), a_class):
+        return True
+    else:
+        return False
