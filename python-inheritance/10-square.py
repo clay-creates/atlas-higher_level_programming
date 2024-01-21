6f29d9c0207cdb567f6db3d3db8397c33cc6cb5f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-21 22:11:18 +0000

    make 10-square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module holds an instance of the Square subclass inheriting fro Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class inheriting from Rectangle -> BaseGeometry
+
+    Args:
+        Rectangle (class): superclass to Square, subclass of BaseGeometry
+    """
+    def __init__(self, size):
+        """
+        Initialization of Square class
+
+        Args:
+            size (int): size of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates area of Square
+
+        Returns:
+            int: size of square
+        """
+        return (self.__size ** 2)
