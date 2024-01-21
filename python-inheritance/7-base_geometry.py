26f9a8c5a90171a1194d46803824524953afe390
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-21 01:28:01 +0000

    make 7-file

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module creates a new class: BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        Method to calculate area of given geometry
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Method to validate values as integer
+
+        Args:
+            name (string): name of variable
+            value (any): value to check if is int
+
+        Raises:
+            TypeError: name must be int
+            ValueError: name must be > 0
+        """
+        if not isinstance(name, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
