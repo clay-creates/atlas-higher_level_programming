3dd35d230936998010f5a40d924ca3ec0f78dc11
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 22:54:12 +0000

    test with add_item.json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,10 +11,10 @@ load_from_json = __import__('6-load_from_json_file').load_from_json_file
 if __name__ == "__main__":
 
     try:
-        arg_list = load_from_json("add.json")
+        arg_list = load_from_json("add_item.json")
     except FileNotFoundError:
         arg_list = []
 
     arg_list.extend(sys.argv[1:])
 
-    save_to_json(arg_list, "add.json")
+    save_to_json(arg_list, "add_item.json")
