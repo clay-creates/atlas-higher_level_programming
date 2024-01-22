e56af4f998362ced45dcc825d5a350e26c60003e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 04:58:22 +0000

    make 5-file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module implements the save_to_json_file method
+"""
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    Method to write an object to file using json representation
+
+    Args:
+        my_obj (object): object to write to file
+        filename (string): name of file to write to
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
