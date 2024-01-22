323c7aa7ea43bbb76edfd4b2d964ad9917a1470c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 22:42:24 +0000

    if loop to append to list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
         arg_list = load_from_json("add.json")
     except FileNotFoundError:
         arg_list = []
-
-    arg_list.extend(sys.argv[1:])
+    
+    for arg in sys.argv[1:]:
+        arg_list.append(arg)
+    
     save_to_json(arg_list, "add.json")
