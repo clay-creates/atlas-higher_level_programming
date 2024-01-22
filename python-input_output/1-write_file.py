b9362c438839714e8719b0c9b0d2230e3e940f8b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 03:38:18 +0000

    make 1-file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains impolementation of write_file method
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Method writes to given file and gives number of characters written
+
+    Args:
+        filename (str, optional): name of file to write to. Defaults to "".
+        text (str, optional): text to write. Defaults to "".
+
+    Returns:
+        int: return number of characters written
+    """
+    with open(filename, encoding="uft-8") as file:
+        file.write(text)
+        return len(text)
+        
