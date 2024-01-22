9f57b8a130c2b8bdc574ab8133dee223354cad3f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 03:53:57 +0000

    make 3-file

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module implements the to_json_string method
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Method returning JSON representation of object
+
+    Args:
+        my_obj (string): object to get json representation
+
+    Returns:
+        json: json representation of given object
+    """
+    return json.load(my_obj)
