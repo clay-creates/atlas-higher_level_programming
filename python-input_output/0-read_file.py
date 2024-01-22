85470b9aa5e2d41f1ca057e10db1819526a56f79
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 03:00:25 +0000

    make 0-file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+This module defines the read_file method
+"""
+
+
+def read_file(filename=""):
+    """
+    Method reads a file and prints it to stdout
+
+    Args:
+        filename (str, optional): UTF8 file. Defaults to "".
+    """
+    with open('UTF8') as filename:
+        print(filename)
