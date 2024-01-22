74ace19c22ae7ba8ee6963e2cebb289c5e30ef6b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 23:12:55 +0000

    make 8-file

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,8 +2,16 @@
 """
 This module implements the class_to_json method
 """
-import json
 
 
 def class_to_json(obj):
-    
+    """
+    Method that returns dictionary description for json serialization 
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return obj.__dict__
