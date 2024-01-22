96b289d1189e0dbfeff72bf3afac0fc340485f3a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-22 05:07:58 +0000

    make 6-file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module implements the load_from_json_file method
+"""
+
+
+import json
+
+def load_from_json_file(my_obj, filename):
+    """
+    Method creates an object from a JSON file
+
+    Args:
+        my_obj (object): object to be made from json
+        filename (string): name of file to get json from
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        my_obj = json.load(file)
+        return my_obj
