db7bf55dfcf37b9e086d4cd6d9369c676c47d51b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-23 00:01:33 +0000

    document and rm exceptions for now

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,10 +7,14 @@ This module implements the Student class
 class Student:
 
     def __init__(self, first_name, last_name, age):
-        if not isinstance((first_name, last_name), str):
-            raise TypeError("first_name and last_name must be a string")
-        if not isinstance(age, int):
-            raise TypeError("age must be an integer")
+        """
+        Initializing Student with firstname, lastname, and age
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
