e9a89579f451e44c8e10ddb17ad1b7be0ac22883
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-25 19:50:54 +0000

    class documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,12 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Implementation of Rectangle class
+
+    Args:
+        Base (class): parent class to Rectangle
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
