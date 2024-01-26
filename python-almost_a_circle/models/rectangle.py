347e1b67a7e8c046c74636d039cc62f6c8e4f804
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-26 04:43:32 +0000

    add if condition in rectangle for sqaure validation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,19 +25,21 @@ class Rectangle(Base):
             id (int, optional): id number of instance. Defaults to None.
         """
         super().__init__(id)
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = width
+        if width is not None:
+            if not isinstance(width, int):
+                raise TypeError("width must be an integer")
+            elif width <= 0:
+                raise ValueError("width must be > 0")
+            else:
+                self.__width = width
 
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = height
+        if height is not None:
+            if not isinstance(height, int):
+                raise TypeError("height must be an integer")
+            elif height <= 0:
+                raise ValueError("height must be > 0")
+            else:
+                self.__height = height
 
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
