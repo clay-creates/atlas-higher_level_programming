d3dc137179eb21b8aae60b0c6fa00b4b37943108
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-01-30 03:36:04 +0000

    fix failures

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -43,7 +43,7 @@ class TestRectangle(unittest.TestCase):
     def test_rectangle_methods(self):
         r = Rectangle(1, 2, 3, 4)
         self.assertEqual(r.area(), 2)
-        self.assertEqual(str(r), "[Rectangle] (11) 3/4 - 1/2")
+        self.assertEqual(str(r), "[Rectangle] (16) 3/4 - 1/2")
 
     def test_update(self):
         r = Rectangle(1, 2, 3, 4)
@@ -57,7 +57,7 @@ class TestRectangle(unittest.TestCase):
     def test_to_dictionary(self):
         r = Rectangle(1, 2, 3, 4)
         d = r.to_dictionary()
-        self.assertEqual(d, {'id': 14, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual(d, {'id': 19, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
 
     def test_create(self):
         r = Rectangle.create(id=1, width=2, height=3, x=4, y=5)
