9e600a6f8a9ba83b7f52e8f8f0f8089448fc60f3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-16 17:08:54 +0000

    rm extra VALUES

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,4 @@
 -- CREATE SECOND_TABLE AND ADD MULTIPLE ROWS
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
-VALUES (1, "John", 10),
-VALUES (2, "Alex", 3),
-VALUES (3, "Bob", 14),
-VALUES (4, "George", 8);
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
