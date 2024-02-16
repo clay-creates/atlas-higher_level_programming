89ab49110863f2ba544f70672168d3f52ed95538
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-16 20:02:00 +0000

    EXISTS

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- LIST PRIVILEGES OF SELECTED USERS
-SHOW GRANTS FOR 'user_0d_1'@'localhost' AND 'user_0d_2'@'localhost';
+SHOW GRANTS IF EXISTS FOR 'user_0d_1'@'localhost' AND 'user_0d_2'@'localhost';
