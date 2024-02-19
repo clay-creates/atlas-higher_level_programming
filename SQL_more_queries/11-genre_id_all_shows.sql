e828e9a20c68c22821e6a454d2bfc8df61228273
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 01:13:17 +0000

    extra comma

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- LIST ALL SHOWS IN HBTN_0D_TVSHOWS
-SELECT tv_shows.title, tv_show_genres.genre_id,
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
