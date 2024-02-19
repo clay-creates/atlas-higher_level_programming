0d243e53b0bf75922f92c3b55fff89b6f3837f27
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 01:36:31 +0000

    rewrite

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- LIST ALL GENRES AND DISPLAY NUMBER OF SHOWS LINKED TO EACH
-SELECT tv_show_genres, COUNT(*) AS number_of_shows
-FROM hbtn_0d_tvshows
-GROUP BY tv_show_genres
-HAVING COUNT(*) > 0
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_genres.id
+WHERE COUNT(*) > 0
 ORDER BY number_of_shows ASC;
