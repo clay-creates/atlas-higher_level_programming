a369391a2aac2c2d0504d2e51a9c79faa887dd01
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 16:11:19 +0000

    make 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- LISTS ALL COMEDY SHOWS IN DATABASE
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
