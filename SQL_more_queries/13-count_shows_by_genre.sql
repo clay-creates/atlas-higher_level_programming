ac40ac7344e50c33fbef00be614e594fe59d5d31
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 01:39:54 +0000

    genre id from show id

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
