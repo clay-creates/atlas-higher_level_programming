e0d46edebc5269b16cef9a7cc111ddb857e22dca
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 00:38:26 +0000

    MAKE 9

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- LIST ALL CITIES IN HBTN_0D_USA
+SELECT cities.id, cities.name, (
+    SELECT states.name FROM states WHERE states.id = cities.state_id
+) AS state_name
+FROM cities
+ORDER BY cities.id ASC;
