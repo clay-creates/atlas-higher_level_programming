53e07c844595cb8218d3db1e21627aaceb5cdbf1
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 20:08:14 +0000

    make 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+List all states with name starting with N, using MySQLdb
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    usrname = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', user=usrname, passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE state.name LIKE 'N' ORDER BY states.id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
