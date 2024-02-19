dc166b150109434080d3542262b3a6a3205f96da
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 00:58:01 +0000

    typo 2

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- LIST SHOWS IN HBTN_0D_TVSHOWS
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show.state_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
