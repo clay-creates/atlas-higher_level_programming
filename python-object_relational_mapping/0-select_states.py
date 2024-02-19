6e4430cca15e1070dd4305178e21e6d330f381cb
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-19 19:49:44 +0000

    make 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+import MySQLdb
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
