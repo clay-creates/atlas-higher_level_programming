e20f3d3ebd118e7bb98a3a61131b798f4206bed7
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-20 20:43:00 +0000

    paremeter query, execute with tuple

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,15 +13,16 @@ if __name__ == "__main__":
     usr = sys.argv[1]
     pw = sys.argv[2]
     database = sys.argv[3]
+    state = sys.argv[4]
 
     db = MySQLdb.connect(host='localhost', user=usr, passwd=pw, db=database)
 
     cursor = db.cursor()
     query = """SELECT * FROM states
-    WHERE BINARY name = '{}'
-    ORDER BY states.id ASC""".format(sys.argv[4])
+    WHERE BINARY name = %s
+    ORDER BY states.id ASC"""
 
-    cursor.execute(query)
+    cursor.execute(query, (state))
     for r in cursor.fetchall():
         print(r)
 
