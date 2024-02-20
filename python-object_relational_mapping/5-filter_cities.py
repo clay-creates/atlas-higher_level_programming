b80ab4581cf8ea7439c37982ceb89dc96ec30e21
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-20 21:02:09 +0000

    make 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Display all cities of state specified by input,
+safe from SQL injections, using MySQLdb
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    usr = sys.argv[1]
+    pw = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', user=usr, passwd=pw, db=database)
+    cursor = db.cursor()
+
+    query = """SELECT cities.id, cities.name
+    FROM cities
+    JOIN states
+    ON states.id = cities.state_id
+    WHERE state.name = %s
+    ORDER BY cities.id ASC"""
+
+    cursor.execute(query, (state,))
+    for r in cursor.fetchall():
+        print(r)
+
+    cursor.close()
+    db.close()
