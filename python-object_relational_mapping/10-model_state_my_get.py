1022e1ea0eba8675b646e1c9860ab37a646ef2d8
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-21 00:41:42 +0000

    make 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Prints State obj with name passed as arg, using SQLAlchemy
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    dbs = sys.argv[3]
+    searchname = sys.argv[4]
+    host = "localhost"
+    url = "mysql+mysqldb"
+
+    connection = "{}://{}:{}@{}:3306/{}".format(url, usr, pwd, host, dbs)
+
+    engine = create_engine(connection)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(State).where(State.name == searchname).all()
+    if results is None:
+        print("Not found")
+    else:
+        print("{}".format(results.id))
