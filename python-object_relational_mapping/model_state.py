3f9b7d990232ad351cc576f89b1ec5c6a150f630
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-21 00:01:37 +0000

    make model_state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Define State class and create instance of Base,
+using SQLAlchemy
+"""
+
+from sqlalchemy import Column, String, Integer, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State -> Base : Contains the definition of the State database
+
+    Args:
+        Base (class): declarative_base()
+    """
+    __tablename__ = "states"
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String(128), nullable=False)
