987457d3728c648e8f6ad36d57b29894ec22278a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-02-21 00:24:46 +0000

    make 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Prints first State object from database, using SQLAlchemy
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    dbs = sys.argv[3]
+    host = "localhost"
+    url = "mysql+mysqldb"
+
+    connection = "{}://{}:{}@{}:3306/{}".format(url, usr, pwd, host, dbs)
+
+    engine = create_engine(connection)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(State).order_by(State.id).first()
+    for r in results:
+        print("{}: {}".format(r.id, r.name))
