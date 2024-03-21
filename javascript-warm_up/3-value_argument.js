4bf4e04824a6ab7bf6724858931d5c0c6cecf5f0
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-21 18:30:02 +0000

    fix 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice();
+const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log('No argument');
+    console.log("No argument");
 } else {
-  console.log(args[1]);
+    console.log(args[0]);
 }
