2898f612cb4a1054126911c65c148a2a7dd22f39
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-22 22:35:11 +0000

    testing with empty block

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (!Number.isInteger(w) || w <= 0) {
-            return Rectangle{ };
-        }
-        if (!Number.isInteger(h) || h <= 0) {
-            return Rectangle{ };
-        }
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if (!Number.isInteger(w) || w <= 0) {
     }
+    if (!Number.isInteger(h) || h <= 0) {
+    }
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
