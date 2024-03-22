4cc44477cae31eddd4a8d0905f40d1bd6439c3c7
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-22 22:44:35 +0000

    comment for semi

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,6 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w < 1 || typeof (w) === 'undefined' || h < 1 || typeof (h) === 'undefined') {
+      // pass
     } else {
       this.width = w;
       this.height = h;
