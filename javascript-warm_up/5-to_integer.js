aa17c0309c46500c8824300fe04f6165a593efd6
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-22 19:08:37 +0000

    5-file

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const num = Number(arg1)
+
+if (Number.isInteger(num)) {
+    console.log(`My number: ${num}`);
+} else {
+    console.log("Not a number");
+}
