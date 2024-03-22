03a75cbcd06972d5a05e32ece0b8557ae7bc84f2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-22 19:41:55 +0000

    fix loop in 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ const sizeInt = parseInt(size, 10);
 if (isNaN(sizeInt)) {
   console.log('Missing size');
 } else {
-  for (let y = 0; y < sizeInt; y++) {
+  for (let x = 0; x < sizeInt; x++) {
     let line = '';
     for (let y = 0; y < sizeInt; y++) {
       line += 'X';
