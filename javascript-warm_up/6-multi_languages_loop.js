0c2cc916f6a3fba36360dac143c1ec048c12a38b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-22 19:14:54 +0000

    semi

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 const lineArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < lineArray.length; i++) {
-    console.log(lineArray[i]);
+  console.log(lineArray[i]);
 }
