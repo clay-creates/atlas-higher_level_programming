9acd5406d614626868225d46bc0f151b87cc3509
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-22 22:37:18 +0000

    set undefined

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,10 @@
 class Rectangle {
   constructor (w, h) {
     if (!Number.isInteger(w) || w <= 0) {
+      typeof (w) === 'undefined';
     }
     if (!Number.isInteger(h) || h <= 0) {
+      typeof (h) === 'undefined';
     }
     this.width = w;
     this.height = h;
