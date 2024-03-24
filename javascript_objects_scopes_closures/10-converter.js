e9b36ea2df4812306b4cee953d2d0a4d71899862
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-24 13:28:09 +0000

    documentation

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
+/*
+Converter defines a function that returns another function,
+the function that is returned takes a number as an argument
+and then the results are returned via the toString method.
+*/
+
 exports.converter = function (base) {
   return function (num) {
     return num.toString(base);
--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 
+/*
+logMe begins with an IIFE (Immediately Invoked Function Expression),
+that creates an internal variable: count. This variable can be reached
+by the nested function created that prints the function call count
+and the item passed to the nested function, then increments the count.
+
+Creating this internal variable using IIFE allows for persisting memory
+of the state of the count variable, while also making the count variable
+inaccessable from outside of the IIFE.
+*/
+
 exports.logMe = (function () {
   let count = 0;
   return function (item) {
