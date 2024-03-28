f52fd3d8a398c7f5211abc1e6b52c8aff4e8bd6c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-28 22:19:30 +0000

    correct condition checking for char id

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,19 +7,18 @@ const apiPath = process.argv[2];
 let count = 0;
 
 request(apiPath, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        const data = JSON.parse(body);
-        for (const film of data.results) {
-            for (const character of film.characters) {
-                const tokens = character.split('/');
-                if (tokens === '18') {
-                    count++
-                }
-                console.log(tokens);
-            }
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    for (const film of data.results) {
+      for (const character of film.characters) {
+        const tokens = character.split('/');
+        if (tokens[5] === '18') {
+          count++;
         }
+      }
     }
-    console.log(count);
-})
+  }
+  console.log(count);
+});
