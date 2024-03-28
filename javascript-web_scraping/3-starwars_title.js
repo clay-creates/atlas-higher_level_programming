9893856775fb14e0c9d93f5bd73892d096c2d43f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-28 21:52:18 +0000

    correct apipath

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,13 +3,13 @@
 const request = require('request');
 
 const movieID = process.argv[2];
-const apiPathRequest = 'https://swapi-api.hbtn.io/api/films/:id' + movieID;
+const apiPathRequest = `https://swapi-api.hbtn.io/api/films/${movieID}`;
 
 request(apiPathRequest, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        const data = JSON.parse(body);
-        console.log(data.title);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
