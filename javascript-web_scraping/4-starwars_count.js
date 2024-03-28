4dfff7d5c932dca4218e9e680b3bdeef52187830
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-28 22:10:35 +0000

    4-file

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiPath = process.argv[2];
+
+let count = 0;
+
+request(apiPath, function (error, response, body) {
+    if (error) {
+        console.error(error);
+    } else {
+        const data = JSON.parse(body);
+        for (const film of data.results) {
+            for (const character of film.characters) {
+                const tokens = character.split('/');
+                if (tokens === '18') {
+                    count++
+                }
+            }
+        }
+    }
+    console.log(count);
+})
