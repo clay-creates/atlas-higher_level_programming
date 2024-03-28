acfde1fde1d01b624106ebae0ac5118c67ca23d2
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-28 23:10:19 +0000

    make 6, may need testing

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiPath = process.argv[2];
+
+exports.tasksCompleted = function () {
+  request(apiPath, { json: true }, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const completedTasks = body.filter(task => task.completed);
+    const completedTasksByUser = {};
+
+    completedTasks.forEach(task => {
+      const userId = task.userId;
+      completedTasksByUser[userId]++;
+    });
+    for (const userId in completedTasksByUser) {
+      console.log(`'${userId}': ${completedTasksByUser[userId]}`);
+    }
+  });
+};
